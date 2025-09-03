bb142d7b8509579a44cda6ad17c8f68808f80d03
TOKJ <tokj0001@stud.ek.dk>
2025-09-03 10:55:40 +0200

    Commit01

--- src/Tivoli01Package/Tivoli01.java
+++ src/Tivoli01Package/Tivoli01.java
@@ -1,4 +1,13 @@
 package Tivoli01Package;
 
 public class Tivoli01 {
+    private String name;
+    private int height;
+
+    public Ride(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+
 }
