46f8c7d47820cd3f5da5f5b3ac4925d226d1a2ca
Namirah0310 <nare0001@stud.ek.dk>
2025-09-03 11:49:49 +0200

    commit03

--- src/Tivoli01Package/Tivoli01.java
+++ src/Tivoli01Package/Tivoli01.java
@@ -4,10 +4,16 @@ public class Tivoli01 {
     private String name;
     private int height;
 
-    public Ride(String name, int height) {
+
+
+    public Tivoli01(String name, int height) {
         this.name = name;
         this.height = height;
     }
 
-
+    public static void createRider() {
+        //Scanner input = new Scanner(System.in);
+        Tivoli01 r1 = new Tivoli01("First Ride",130);
+        System.out.println(r1.name + " has a minimum height requirement of " + r1.height + " cm.");
+    }
 }
